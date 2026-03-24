345a86bad490b0601df9b22077359933d0c10f8e
YooSoMin <somin3994@naver.com>
2026-03-24 15:12:27 +0900

    fix: 오프라인 결제 토큰 테이블 추가

--- app/backend/src/main/java/com/akku/backend/domain/bank/entity/Merchant.java
+++ app/backend/src/main/java/com/akku/backend/domain/bank/entity/Merchant.java
@@ -1,14 +1,14 @@
 package com.akku.backend.domain.bank.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "merchant")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Merchant {
 
     @Id
